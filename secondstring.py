a9b5131b87d5843f6c68d7219a11d36a5b5c5b5a
Jonathon <jonnygrealish64@gmail.com>
2021-03-10 02:57:35 +0000

    Weekly Tasks

---
+++ secondstring.py
@@ -0,0 +1,5 @@
+# This program asks the user to input a string.
+# The string then outputs every second letter in reverse order.
+# Author: Jonathon Grealish
+
+# The quick brown fox jumps over the lazy dog.
---
+++ weekday.py
@@ -0,0 +1,8 @@
+# Write a program that outputs whether or not today is a weekday.
+# (You will need to search the web to find how you work out what day it is)
+# An example of running this program on a Thursday is given below.
+# Yes, unfortunately today is a weekday.
+# It is the weekend, yay!
+# Author: Jonathon Grealish
+
+
