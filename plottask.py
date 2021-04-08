6140d82a39439bf1fef298050b40ce49264d1058
Jonathon <jonnygrealish64@gmail.com>
2021-04-08 22:30:40 +0100

    completed squareroot and plottask

--- plottask.py
+++ plottask.py
@@ -1,5 +1,19 @@
-# This displays a plot of the functions f(x) = x, g(x) = x^2 and h(x) = x^3
+# This displays a plot of the functions:
+# f(x) = x, g(x) = x^2 and h(x) = x^3.
 # in the range [0, 4] on the one set of axes.
 # Author: Jonathon Grealish
 # References:
-# TBC
+# https://matplotlib.org/2.0.2/users/pyplot_tutorial.html
+
+import matplotlib.pyplot as plt # a collection of command style functions.
+import numpy as np              # a collection used to work with numerical data.
+
+# The plot() command will take an arbitrary pair of arguments (x and y).
+# There is an optional third argument, the format string.
+# This string indicates the color and line type of the plot, it concatenates a color string with a line style string.
+# The axis() command takes an array of [xmin, xmax, ymin, ymax].
+# All sequences are converted to numpy arrays internally:
+
+t = np.arange(0., 5., 0.2)
+plt.plot(t, t, 'r--', t, t**2, 'b--', t, t**3, 'g--')
+plt.show()
--- squareroot.py
+++ squareroot.py
@@ -1,14 +1,40 @@
-# Write a program that takes a positive floating-point number as input
+# This program that takes a positive floating-point number as input
 # and outputs an approximation of its square root.
-# You should create a function called <tt>sqrt</tt> that does this.
-# I am asking you to create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x).
-# This is to demonstrate that you can research and code a process
-# (If you really needed the square root you would use one of the above methods).
-# I suggest that you look at the newton method at estimating square roots.
-# This is a more difficult task than some of the others, but will be marked equally,
-# so only do as much work on this as you feel comfortable.
-# Please enter a positive number: 14.5
-# The square root of 14.5 is approx. 3.8.
 # Author: Jonathon Grealish
 # References:
-# TBC
+# https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+# https://surajregmi.medium.com/how-to-calculate-the-square-root-of-a-number-newton-raphson-method-f8007714f64
+
+# We take a reasonable guess (approximate root) for the square root.
+# Add the approximate root with the original number, divided by the approximate root, and divide by 2.
+# Continue until the difference in the approximate root, along the iterations is less than the desired value (or precision value).
+
+# n = 16, x0 = 16, we determine the next approximation by the formula:
+# x_n+1 = (x_i + n/x_i) / 2
+# Continue the iterative process until the root is found to the desired accuracy.
+# Iteration #1: x1 = (16 + 16/16) / 2 = 8.5
+# Iteration #2: x2 = (8.5 + 16/8.5) / 2 = 5.19117647059
+# Iteration #3: x3 = (5.19117647059 + 16/5.19117647059) / 2 = 4.13666472255 
+# Iteration #4: x4 = (4.13666472255 + 16/4.13666472255) / 2 = 4.0022575248
+# Iteration #5: x5 = (4.0022575248 + 16/4.0022575248) / 2 = 4.00000063669
+# Iteration #6: x6 = (4.00000063669 + 16/4.00000063669) / 2 = 4 
+ 
+def squareRoot(n, l):
+    x = n # assuming the sqrt of n as n only
+    count = 0 # to count the number of iterations 
+  
+    while(1):
+        count += 1 # increment the count each time going through the loop 
+        root = 0.5 * (x + (n / x)) # calculate more closed x
+        if (abs(root - x) < l): # check for closeness
+            break  
+        x = root # update root
+    return root 
+  
+# Driver code 
+if __name__ == "__main__" : 
+  
+    n = int(input("Please enter a positive number: "))
+    l = 0.00001 
+  
+    print("The square root of", n, "is approximately:", squareRoot(n, l))
